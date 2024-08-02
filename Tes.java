2aebe7fa6f5fd019017c299a5567b3b819bb0726
Felliciayoja <fellicia_zhafirah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:17:07 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,19 @@
+public class Tes {
+public static void main(String[] args) {
+    String nama= "Felli";
+    String asalKota= "Malang";
+    int usia=15;
+    int tinggiBadan=157;
+    double beratBadan=50.00;
+    char jenisKelamin= 'P';
+    boolean status = false;
+
+    System.out.println("Nama: " +nama);
+    System.out.println("Asal Kota: " +asalKota);
+    System.out.println("Tinggi Badan: " +tinggiBadan);
+    System.out.println("Berat Badan: " +beratBadan);
+    System.out.println("Jenis Kelamin: " +jenisKelamin);
+    
+   }
+}
+   
